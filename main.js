640d1952c369a2dfcfe0e0f6f6e5e4e2844e07b2
Cristian Franco <cristianfrancod@gmail.com>
2020-08-13 17:40:01 -0600

    js moved to js file

---
+++ main.js
@@ -0,0 +1,56 @@
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; } 
+  }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable
+function disableScroll() {
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+// call this to Enable
+function enableScroll() {
+  window.removeEventListener('DOMMouseScroll', preventDefault, false);
+  window.removeEventListener(wheelEvent, preventDefault, wheelOpt); 
+  window.removeEventListener('touchmove', preventDefault, wheelOpt);
+  window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+            function myFunction() {
+              var x = document.getElementById("myLinks");
+              var men1 = document.getElementById("icon-menu1");
+              var men2 = document.getElementById("icon-menu2");
+
+              if (x.style.display === "block") {
+                x.style.display = "none";
+                men2.style.display = "none";
+                men1.style.display = "block"
+                enableScroll();
+              } else {
+                disableScroll();
+                x.style.display = "block";
+                men1.style.display = "none"
+                men2.style.display = "block"
+              }
+            }
